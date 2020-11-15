a6c07d9a8b203089eca60659152b8844e4563110
astro_dev <isajaffarali@gmail.com>
2020-11-15 22:56:57 +0300

    Finished Site

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -36,7 +36,7 @@ weatherForm.addEventListener('submit', (e) => {
     feelslike.textContent = (``);
 
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = (`${data.error}`);
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 
 // Define paths for Express config
@@ -88,6 +89,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
